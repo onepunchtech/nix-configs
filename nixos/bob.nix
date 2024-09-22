c3f213f987ef54debfab6c0eed82ccb46e2c5711
Michael Whitehead <onepunchlinux@gmail.com>
2024-09-22 15:35:39 -0600

    update bob

--- nixos/bob.nix
+++ nixos/bob.nix
@@ -1,7 +1,30 @@
+{pkgs, config, ...}:
 {
   imports = [
-    ./hardware/base-hardware.nix
     ./lib/base.nix
-    ./lib/amdgpu.nix
+    ./hardware/base-hardware.nix
   ];
+
+  networking.hostName = "bob";
+
+  hardware.opengl = {
+    enable = true;
+  };
+
+  boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
