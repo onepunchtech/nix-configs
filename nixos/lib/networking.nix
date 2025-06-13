194810255583f4fc96077f458821416ccad45a6d
Michael Whitehead <onepunchlinux@gmail.com>
2025-06-12 23:58:57 -0600

    wip

--- nixos/lib/networking.nix
+++ nixos/lib/networking.nix
@@ -1,13 +1,8 @@
 { ... }:
 {
-  services.resolved.enable = true;
-
   networking = {
-    nameservers = [ "10.10.10.11" ];
-    useDHCP = false;
     networkmanager = {
       enable = true;
-      dns = "systemd-resolved";
     };
   };
 }
